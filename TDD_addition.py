6bad4c62542c4dd1ed10473e919112c9275f2b13
Nidhi-3344 <nidhi_sharma1@epam.com>
2025-08-17 23:07:46 +0530

    Create TDD_addition.py

---
+++ TDD_addition.py
@@ -0,0 +1,25 @@
+import unittest
+
+class TestAddition(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(2, 3), 5)
+    
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-2, -3), -5)
+    
+    def test_add_positive_and_negative(self):
+        self.assertEqual(add(2, -3), -1)
+    
+    def test_add_zero(self):
+        self.assertEqual(add(0, 5), 5)
+        self.assertEqual(add(5, 0), 5)
+    
+    def test_add_floats(self):
+        self.assertAlmostEqual(add(2.5, 3.7), 6.2, places=2)
+
+# Placeholder for the add function
+def add(a, b):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
